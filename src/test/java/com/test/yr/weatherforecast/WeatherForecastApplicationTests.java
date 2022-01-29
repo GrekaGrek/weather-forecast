b92079b5ea336900ac2ae4262c8ca437d474f3ef
Anatolijs Bikmulins <Anatolijs.Bikmulins@klp.no>
2022-01-29 16:00:33 +0100

    init commit

---
+++ src/test/java/com/test/yr/weatherforecast/WeatherForecastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.yr.weatherforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherForecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
